64da6de88c121da062f975239267f65262529ef4
Jangho Lee <jangho.lee.92@gmail.com>
2026-04-18 03:26:35 -0400

    Error fix

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,15 +2,11 @@ import rss from "@astrojs/rss"
 import { getCollection } from "astro:content"
 import { SITE } from "@consts"
 
-type Context = {
-  site: string
-}
-
-export async function GET(context: Context) {
-	const posts = await getCollection("blog")
+export async function GET(context: any) {
+  const news = await getCollection("news")
   const projects = await getCollection("projects")
 
-  const items = [...posts, ...projects]
+  const items = [...news, ...projects]
 
   items.sort((a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime())
 
@@ -22,9 +18,7 @@ export async function GET(context: Context) {
       title: item.data.title,
       description: item.data.summary,
       pubDate: item.data.date,
-      link: item.slug.startsWith("blog")
-        ? `/blog/${item.slug}/`
-        : `/projects/${item.slug}/`,
+      link: `/${item.collection}/${item.slug}/`,
     })),
   })
 }
